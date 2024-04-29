00ff512d99d7d89c14d9c493e18cc0d2ad3a3dd5
Hawahi <128416298+hawahi1@users.noreply.github.com>
2024-04-29 17:25:07 +0100

    progress made but having issues with dotnet

--- CMP1903_A2_2324/SevensOut.cs
+++ CMP1903_A2_2324/SevensOut.cs
@@ -6,15 +6,40 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A2_2324
 {
-    internal class SevensOut
+    internal class SevensOutPlayerOne // Creating the class for the Sevens out game.
     {
-       public static void Sevens()
+        public static int SevensPlayerOne() // Creating the player one method
         {
             Die die = new Die();
-            int valueOne = die.DieRoll();
-            int valueTwo = die.DieRoll();
-            int totalValue = valueOne + valueTwo;
-            if (valueOne == valueTwo) { totalValue = (valueOne + valueTwo)*2; };
+            int valueOnePlayerOne = die.DieRoll();
+            int valueTwoPlayerOne = die.DieRoll();
+            int totalPlayerOne = valueOnePlayerOne + valueTwoPlayerOne;
+            if (valueOnePlayerOne == valueTwoPlayerOne) { totalPlayerOne = (valueOnePlayerOne + valueTwoPlayerOne)*2; };
+            return totalPlayerOne;
+            
+        }
+
+    }
+    internal class SevensOutPlayerTwo
+    {
+        public static int SevensPlayerTwo() // Player two method
+        {
+            Die die = new Die();
+            int valueOnePlayerTwo = die.DieRoll();
+            int valueTwoPlayerTwo = die.DieRoll();
+            int totalPlayerTwo = valueOnePlayerTwo + valueTwoPlayerTwo;
+            if (valueOnePlayerTwo == valueTwoPlayerTwo) { totalPlayerTwo = (valueOnePlayerTwo + valueTwoPlayerTwo) * 2; };
+            return totalPlayerTwo;
+        }
+
+    }
+    internal class SevensGameLoop
+    {
+        SevensOutPlayerOne SevensOne = new SevensOutPlayerOne();
+        SevensOutPlayerTwo SevensTwo = new SevensOutPlayerTwo();
+        public static void GameLoop(SevensOutPlayerTwo, SevensOutPlayerOne)
+        {
+
         }
     }
 }
